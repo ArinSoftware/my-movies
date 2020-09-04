83b5e5991f32b02f23dbab3016e2f41cd68e5501
Arin <arinyazilim@gmail.com>
2020-09-04 17:20:08 +0300

    Ders 18

--- src/components/App.js
+++ src/components/App.js
@@ -5,58 +5,18 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
 
     state = {
-        movies: [
-            {
-                "id": 1,
-                "name": "The Flash",
-                "rating": 8.3,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-            },
-        
-            {
-                "id": 2,
-                "name": "Interstellar",
-                "rating": 6.8,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-            },
-        
-            {
-                "id": 3,
-                "name": "Arrow",
-                "rating": 7.9,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-            },
-
-            {
-                "id": 4,
-                "name": "Rogue",
-                "rating": 7.4,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg"
-            },
-
-            {
-                "id": 5,
-                "name": "Project Power",
-                "rating": 6.7,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg"
-            },
-
-            {
-                "id": 6,
-                "name": "Superman",
-                "rating": 7.6,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-            }
-            
-        ],
+        movies: [],
 
         searchQuery: ""
+    }
+
+    async componentDidMount() {
+        const baseURL = "http://localhost:3002/movies";
+        const response = await fetch(baseURL);
+        console.log(response)
+        const data = await response.json();
+        console.log(data)
+        this.setState({movies: data})
     }
 
 
